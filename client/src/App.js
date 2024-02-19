d6d8e6269cfda438d38273669c8d35b73d017446
dinesh3636 <dineshmoorthy66369@gmail.com>
2024-02-19 22:46:41 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -13,11 +13,7 @@ function App() {
     <>
     <ComplexNavbar />
     <Routes>
-<<<<<<< HEAD
- 
-=======
         <Route path="/" element={<GoogleMap/>} />
->>>>>>> 38bf41a808095855e1f95fa6bce6fe9a21e25d00
         <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/profile" element={<Profile/>} />
         <Route path="/settings" element={<Settings/>} />
