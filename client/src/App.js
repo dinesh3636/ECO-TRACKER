38bf41a808095855e1f95fa6bce6fe9a21e25d00
johnjehiel <jnjehiel@gmail.com>
2024-02-19 21:37:14 +0530

    Google Maps Api Integrated

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./Components/Pages/Dashboard/Dashboard"
 import Profile from "./Components/Pages/Profile/Profile"
 import Settings from './Components/Pages/Settings/Settings'
 import Carpooling from './Components/Pages/Carpooling/Carpooling';
+import GoogleMap from './Components/GoogleMap/GoogleMap';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <>
     <ComplexNavbar />
     <Routes>
+        <Route path="/" element={<GoogleMap/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/profile" element={<Profile/>} />
         <Route path="/settings" element={<Settings/>} />
